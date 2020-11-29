7fc680300771717ddca9c85b46b952adb27a46c9
daniel alvarado <daniel.tintin86@gmail.com>
2020-11-29 17:32:59 -0500

    Inicio del curso - Columnas y estilos

---
+++ android/app/src/main/java/devcatpro/flutter_app/MainActivity.java
@@ -0,0 +1,6 @@
+package devcatpro.flutter_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final estiloTexto = new TextStyle(fontSize: 30);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Titulo'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Número de tabs:",
+                style: estiloTexto,
+              ),
+              Text(
+                "0",
+                style: estiloTexto,
+              ),
+            ],
+          ),
+        ));
+  }
+}
