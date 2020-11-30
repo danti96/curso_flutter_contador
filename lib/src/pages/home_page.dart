f5d1553088d23646ba582afb1ec8a0d751ffa1f3
daniel alvarado <daniel.tintin86@gmail.com>
2020-11-29 21:03:55 -0500

    realizando contado - problema con el conteo con StateLessWidget

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final estiloTexto = new TextStyle(fontSize: 30);
+  final conteo = 10;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +20,12 @@ class HomePage extends StatelessWidget {
               style: estiloTexto,
             ),
             Text(
-              "0",
+              conteo.toString(),
               style: estiloTexto,
             ),
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
